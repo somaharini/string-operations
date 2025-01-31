ce1091f84d8784eec3b77fc9e55247fb121dda4b
somaharini <somaharini24@gmail.com>
2025-01-31 10:37:38 +0530

    Update Stringoperations.java

--- Stringoperations.java
+++ Stringoperations.java
@@ -2,7 +2,7 @@ public class Stringoperations {
     public static void main(String[] args) {
         String str = "hello";
         System.out.println("Uppercase: " + str.toUpperCase());
-        System.out.println("Length: " + str.length())
+        System.out.println("Length: " + str.length());
         System.out.println("Replaced: " + str.replace('e', 'a'));
         System.out.println("Substring: " + str.substring(1, 4));
     }
